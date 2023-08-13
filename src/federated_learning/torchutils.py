6012d235a15eb457cada7e8061d8193926f26598
Lukas Meinhold <lukas@meinhold.dev>
2023-08-13 15:01:05 +0200

    Implement and test first MNIST models

---
+++ src/federated_learning/torchutils.py
@@ -0,0 +1,11 @@
+import logging
+
+import torch
+
+
+def get_device() -> str:
+    """Return a string representing the device to use for training"""
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    logging.debug(f"Using device '{device}'")
+    return device
+
---
+++ src/models/mnist.py
@@ -0,0 +1,62 @@
+"""Models for MNIST-style 28x28 image classification"""
+
+from torch import nn
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=6,
+                kernel_size=5,
+                stride=1,
+                padding=0),
+            nn.BatchNorm2d(6),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=6,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=0
+            ),
+            nn.BatchNorm2d(16),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.output = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(16 * 4 * 4, 120),
+            nn.LeakyReLU(),
+            nn.Linear(120, 84),
+            nn.LeakyReLU(),
+            nn.Linear(84, 10))
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.output(x)
+        return x
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.m = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28 * 28, 128),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(128),
+            nn.Linear(128, 128),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(128),
+            nn.Linear(128, 10),
+            nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.m(x)
